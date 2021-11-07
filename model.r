4968bce118c1b422bde88401925141cdbc4c5691
zoey1124 <zoey_1124@berkeley.edu>
2021-11-07 01:28:02 -0700

    to build parametric model

--- model.r
+++ model.r
@@ -1,4 +1,15 @@
+# parametric trend + non-parametric seasonality 
 data <- read.csv("data/TSLA.csv")
-data
-t = 1:length(data)
-t
+open <- data$Open
+close <- data$Adj.Close
+close_tail = tail(close, 300)
+plot.ts(close_tail)
+log_data = log(close_tail)
+plot.ts(log_data)
+
+# fit a parametric model
+
+# use differencing to pursue seasonality
+diff = diff(log_data, lag=7)
+second_diff = diff(diff, lag = 1)
+plot.ts(second_diff)
