9e6593cf6c23ccd8378625f66d33b917786196c2
zoey1124 <zoey_1124@berkeley.edu>
2021-11-07 08:23:19 -0800

    write in r markdown

--- model.r
+++ model.r
@@ -1,14 +1,15 @@
 # parametric trend + non-parametric seasonality 
+t = 1:300
 data <- read.csv("data/TSLA.csv")
 open <- data$Open
 close <- data$Adj.Close
 close_tail = tail(close, 300)
 plot.ts(close_tail)
 log_data = log(close_tail)
+log_data = log(log_data)
 plot.ts(log_data)
 
 # fit a parametric model - linear regression
-t = 1:300
 lm <- lm(log_data ~ t)
 res <- resid(lm)
 plot.ts(res)
@@ -17,3 +18,4 @@ plot.ts(res)
 diff = diff(res, lag=5)
 second_diff = diff(diff, lag = 1)
 plot.ts(second_diff)
+
